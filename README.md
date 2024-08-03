68a2b9ec68efb14e417a708f6e97aa431019b459
Mark Howard <bark4mark@gmail.com>
2024-08-03 20:12:09 +0100

    Initial implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# 1 billion row challenge
+
+Notes: I needed to rename the directory from 1brc to one_brc as rust doesn't support the directory starting with a number.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::BTreeMap, fs};
+
+#[derive(Debug)]
+struct Weather {
+    min: f32,
+    max: f32,
+    avg: f32,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    let path = match std::env::args().skip(1).next() {
+        Some(path) => path,
+        None => "measurements.txt".to_owned(),
+    };
+    let contents = fs::read_to_string(path)?;
+    let mut weather_map: BTreeMap<String, Weather> = BTreeMap::new();
+    for line in contents.lines() {
+        let mut iter = line.split(";");
+        let city = iter.next().unwrap();
+        let temp: f32 = iter.next().unwrap().parse().unwrap();
+        let entry = weather_map.entry(city.to_string()).or_insert(Weather { min: temp, max: temp, avg: temp });
+        if temp < entry.min {
+            entry.min = temp;
+        }
+        if temp > entry.max {
+            entry.max = temp;
+        }
+    }
+    for (_city, mut weather) in weather_map {
+        weather.avg = (weather.min + weather.max) / 2.0;
+        println!("{};{};{};{}", _city, weather.min, weather.max, weather.avg);
+    }
+
+    Ok(())
 }
